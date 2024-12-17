053be8556d4707ae6233bfa4f4cfc9aba79ecaf6
Maria Eduarda Nunes <95004623+Madu3304@users.noreply.github.com>
2024-12-17 17:01:32 -0300

    Add files via upload

---
+++ Agenda/Agenda/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agenda
+{
+    public class Program
+    {
+
+        //menu
+
+
+        static void Main(string[] args)
+        {
+            //variaveis de armaznamentos
+           string[] nome = new string[200];
+           string[] email = new string[200];
+
+            int tl = 0;
+            int op = 0;
+
+
+        }
+    }
+}
